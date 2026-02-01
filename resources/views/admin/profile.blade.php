eb2f6a687b1e4a2e5775b3f01c6f8c2e9fd15cfb
relipalor <reliepalor15@gmail.com>
2026-02-01 20:38:28 +0800

    inital commit

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -181,15 +181,7 @@
                                                     </div>
 
                                                     <!-- Last Login -->
-                                                    <div>
-                                                        <label class="block text-sm font-medium text-gray-700 mb-2">
-                                                            <svg class="w-4 h-4 inline mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                                                            </svg>
-                                                            Last Login
-                                                        </label>
-                                                        <div class="text-lg font-semibold text-gray-900 py-2">{{ $admin && $admin->last_login_at ? $admin->last_login_at->format('M d, Y H:i') : 'Never' }}</div>
-                                                    </div>
+                                                   
                                                 </div>
 
                                                 <!-- Edit Mode Buttons (Hidden by default) -->
